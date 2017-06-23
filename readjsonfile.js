f73bee485d6523f80b5ece91a447ded617beadc2
Ryan Hansen <rphansen91@gmail.com>
2017-06-22 20:31:18 -0700

    Promise.all

--- readjsonfile.js
+++ readjsonfile.js
@@ -1,16 +1,21 @@
- 
- var fs = require('fs');
- var prettyjson =require('prettyjson');
+const fs = require('fs');
+const jsonfile = require('./readjsonfile.js');
 
-exports.readfile = function readfile () {
- fs.readFile('CardFile.JSON', function (err, data){
+function readfile (filename) {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filename, function (err, data){
         if (err) {
-            throw err;
-        }    
-        var fileContents = JSON.parse(data)
-        console.log('current json file contents' + fileContents);
-        console.log(prettyjson.render(fileContents));
-        // return fileContents;
+          reject(err)
+        } else {
+          try {
+            var fileContents = JSON.parse(data)
+            resolve(fileContents)
+          } catch (err) {
+            reject({ message: 'Invalid JSON cannot read file ' + filename  })
+          }
+        }
     });
-    
-} 
+  })
+}
+
+module.exports = readfile;
